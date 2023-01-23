f60474829dbfd9303af74a8ac962f3df9a2a28df
LGSDenzo173 <johnyb574@gmail.com>
2023-01-23 13:10:32 +0500

    uploading projects

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import './categories.styles.scss';
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Shirts',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    },
+  ];
+
+  return (
+    <div className="categories-container">
+      {categories.map(({ title, id }) => (
+        <div key={id} className="category-container">
+          <div className="background-image" />
+          <div className="catergory-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
